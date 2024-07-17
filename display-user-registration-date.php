f8f1146f483f5310134fd2539e5fea27c52be0ea
Anthony Hortin <anthony@maddisondesigns.com>
2024-07-17 18:43:16 +1000

    Initial version

---
+++ display-user-registration-date.php
@@ -0,0 +1,108 @@
+<?php
+/*
+Plugin Name: Display User Registration Date
+Plugin URI: http://maddisondesigns.com
+Description: A simple plugin to display the registration date of your Users on the Users List screen and the single User page.
+Version: 1.0.0
+Author: Anthony Hortin
+Author URI: http://maddisondesigns.com
+Text Domain: display-user-registration-date
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see http://www.gnu.org/licenses/gpl-2.0.html.
+*/
+
+class durd_display_user_registration_date_plugin {
+
+	const SETTINGS_NAMESPACE = 'display_user_registration_date';
+	public $timezoneString = '';
+	public $dateFormat = '';
+	public $timeFormat = '';
+
+	/**
+	 * Add the necessary filters to display the registration date and to make the column sortable
+	 */
+	public function __construct() {
+		$this->timezoneString = new DateTimeZone( wp_timezone_string() );
+		$this->dateFormat = get_option( 'date_format' );
+		$this->timeFormat = get_option( 'time_format' );
+
+		if ( is_admin() ) {
+			// Add Filters for User list page
+			add_filter( 'manage_users_columns', array( $this, 'durd_manage_users_columns' ) );
+			add_filter( 'manage_users_custom_column', array( $this, 'durd_manage_users_custom_column' ), 10, 3 );
+			add_filter( 'manage_users_sortable_columns', array( $this, 'durd_manage_users_sortable_columns' ) );
+
+			// Add Actions for User profile page
+			add_action( 'show_user_profile', array( $this, 'durd_display_reg_date' ) );
+			add_action( 'edit_user_profile', array( $this, 'durd_display_reg_date' ) );
+
+		}
+	}
+
+	/**
+	 * Add User Registration Date column to User list page
+	 */
+	public function durd_manage_users_columns( $columns ) {
+		$columns['registration_date'] = 'Registered';
+		return $columns;
+	}
+
+	/**
+	 * Display data for new user registration date column
+	 */
+	public function durd_manage_users_custom_column( $row_output, $column_id_attr, $user ) {
+		$timestamp = get_userdata($user)->user_registered;
+		$date = DateTime::createFromFormat( 'Y-m-d G:i:s', $timestamp, new DateTimeZone( 'UTC' ) );
+		$date->setTimezone( $this->timezoneString );
+
+		if ( !empty( $timestamp ) ) {
+			if ( false === $date ) {
+				return( 'Invalid date format' );
+			} 
+			else {
+				return $date->format( 'Y/m/d g:i:s a T' );
+			}
+		}
+
+		return $row_output;
+	}
+
+	/**
+	 * Make the registration date column sortable
+	 */
+	public function durd_manage_users_sortable_columns( $columns ) {
+		return wp_parse_args( array( 'registration_date' => 'registered' ), $columns );
+	}
+
+	/**
+	 * Display the registration date on the User Profile page
+	 */
+	public function durd_display_reg_date( $user ) {
+		$timestamp = $user->user_registered;
+		$date = DateTime::createFromFormat( 'Y-m-d G:i:s', $timestamp, new DateTimeZone( 'UTC' ) );
+		$date->setTimezone( $this->timezoneString );
+
+		echo '<table class="form-table"><tr>';
+		echo '<th><label for="registration_date">' . esc_html( __( 'User Registration Date', 'display-user-registration-date' ) ) . '</label></th>';
+		echo '<td>';
+		echo '<input type="text" name="registration_date" id="registration_date" value="' . esc_attr( $date->format( $this->dateFormat . ' @ ' . $this->timeFormat . ' T' ) ) . '" class="regular-text" readonly="readonly" />';
+		echo '<p class="description">' . esc_html( __( 'The user registration date cannot be edited.', 'display-user-registration-date' ) ) . '</p>';
+		echo '</td>';
+		echo '</tr></table>';
+	}
+}
+
+$durd_display_user_registration_date = new durd_display_user_registration_date_plugin();
---
+++ readme.md
@@ -0,0 +1,40 @@
+# Display User Registration Date
+Contributors: ahortin  
+Tags: users, users list, registered date, admin, profile  
+Requires at least: 6.4  
+Tested up to: 6.5  
+Stable tag: 1.0.0  
+License: GPLv2 or later  
+License URI: http://www.gnu.org/licenses/gpl-2.0.html  
+
+A simple plugin to display the registration date of your Users on the Users List screen and the single User Profile screen.
+
+## Description
+
+Adds a sortable column with the registration date for each User on the Users list screen, within the WordPress Dashboard. The user registration date will also be shown on the single User Profile screen. Knowing the registration date is especially useful for sites with a large number of users, such as for eCommerce or membership websites.
+
+The registration date will be displayed with respect to the timezone selected within the **Settings > General** screen. The displayed date on the single User Profile page will also honour the time and date formats selected within your settings.
+
+## Installation
+
+1. Upload the 'display-user-registration-date' folder to your '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+## Frequently Asked Questions
+
+### How can I change the format of the registration date?
+The format of the date within the Users list screen is fixed, but you can change the format of the date & time that is displayed on the single User Profile screen, within the *Timezone*, *Date Format* and *Time Format* fields on the **Settings > General** screen.
+
+## Screenshots
+
+1. Users List screen
+![Users List screen](https://maddisondesigns.com/wp-content/uploads/2024/07/display-user-registration-date_screenshot-01.png "Users List screen")
+
+2. User Profile screen
+![User Profile screen](https://maddisondesigns.com/wp-content/uploads/2024/07/display-user-registration-date_screenshot-02.png "User Profile screen")
+
+## Changelog
+
+### 1.0.0
+- Initial version 🎉
